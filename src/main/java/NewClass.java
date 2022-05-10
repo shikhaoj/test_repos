e79cacb598de299fa750da0746665cafd8dc4253
shikhaoj <shikhaoj@gmail.com>
2022-05-10 22:18:11 +0530

    Commit 3

--- src/main/java/NewClass.java
+++ src/main/java/NewClass.java
@@ -2,5 +2,7 @@ public class NewClass {
     public static void function(){
         System.out.println("Hello");
         System.out.println("New Merge");
+        System.out.println("Your another print statement");
     }
 }
+
