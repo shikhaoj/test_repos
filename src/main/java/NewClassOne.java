42a631bf6e9cb86bb60c2a65bdaac3f6c350dcfa
shikhaoj <shikhaoj@gmail.com>
2022-05-10 23:13:21 +0530

    Commit 6

--- src/main/java/NewClassOne.java
+++ src/main/java/NewClassOne.java
@@ -2,5 +2,6 @@ public class NewClassOne {
     public static void features(){
         System.out.println("feature1");
         System.out.println("feature2");
+        System.out.println("feature3");
     }
 }
