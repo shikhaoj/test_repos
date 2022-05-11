a18eddb5c44a14c5f1e75b0cbd39422034af8324
shikhaoj <shikhaoj@gmail.com>
2022-05-11 22:53:08 +0530

    Resolving conflict

---
+++ src/main/java/CherryPickClass.java
@@ -0,0 +1,9 @@
+public class CherryPickClass {
+    public static void cherryMethod(){
+        System.out.println("cherry-pick 1");
+        System.out.println("cherry-pick 2");
+        System.out.println("cherry-pick 3");
+        System.out.println("cherry-pick 4");
+        System.out.println("cherry-pick 5");
+    }
+}
