25f89c3622012ef54b414f1d5543cfd83305e029
shikhaoj <shikhaoj@gmail.com>
2022-05-11 22:51:19 +0530

    Newcommit

--- src/main/java/CherryPickClass.java
+++ src/main/java/CherryPickClass.java
@@ -1,5 +1,10 @@
 public class CherryPickClass {
     public static void cherryMethod(){
         System.out.println("cherry-pick 1");
+<<<<<<< HEAD
+=======
+        System.out.println("cherry-pick 2");
+        System.out.println("cherry-pick 3");
+>>>>>>> 11badf0... third cherry-pick commit
     }
 }
